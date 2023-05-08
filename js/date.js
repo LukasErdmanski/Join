74b1dea0e713d3574f9e97c0d1fb265b9842cacc
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-05-08 10:22:56 +0200

    Limit the selection in date picker only for today and future / Deleted addTaskCopy html

--- js/date.js
+++ js/date.js
@@ -2,18 +2,21 @@
  * The settings of the date picker on the board and add task sub page.
  */
 const DATE_PICKER_SETTINGS = {
-  format: "dd/mm/yyyy",
+  format: 'dd/mm/yyyy',
   autoclose: true,
   calendarWeeks: true,
   clearBtn: true,
   disableTouchKeyboard: true,
+  startDate: new Date()
 };
 
-$(document).ready(() => setDatePickerSettings());
+$(document).ready(() => {
+  setDatePickerSettings();
+});
 
 /**
  * Sets the properties from the 'DATE_PICKER_SETTINGS' JSON for the date picker on the board and add task sub page.
  */
 function setDatePickerSettings() {
-  $(".input-daterange").datepicker(DATE_PICKER_SETTINGS);
+  $('.input-daterange').datepicker(DATE_PICKER_SETTINGS);
 }
